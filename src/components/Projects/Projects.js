0fd6a3597a3bc4b71ea7238c7822fbd19ba16569
MeriemMerbouhi <147644221+MeriemMerbouhi@users.noreply.github.com>
2023-12-10 15:53:35 +0100

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -22,7 +22,7 @@ function Projects() {
               imgPath={MeryShop}
               isBlog={false}
               title="Mery Shop"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              description="Thrilled to introduce my latest e-commerce project! I've meticulously crafted a dynamic shopping website using HTML, CSS, Bootstrap, JavaScript, and jQuery. Experience the live site on my portfolio and explore the source code on GitHub. Stay tuned for updates, and feel free to browse the GitHub repository for an in-depth look. Happy shopping! 🛍️🌐💻"
               ghLink="https://github.com/MeriemMerbouhi/app-stor"
               demoLink="https://meriemmerbouhi.github.io/app-stor/"
             />
@@ -33,7 +33,7 @@ function Projects() {
               imgPath={MeryTech}
               isBlog={false}
               title="Mery Tech"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
+              description="Explore my project, a business services template meticulously crafted with HTML and CSS. This web application provides a professional and responsive design, tailored for showcasing various services. Discover the set features that make this template an ideal solution for businesses. For further details or potential collaboration, please connect with me. 🌐📱💼"
               ghLink="https://github.com/MeriemMerbouhi/service"
               demoLink="https://meriemmerbouhi.github.io/service/"
             />
@@ -44,45 +44,10 @@ function Projects() {
               imgPath={restaurant}
               isBlog={false}
               title="My pizza"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              // ghLink="https://github.com/soumyajit4419/Editor.io"
-              // demoLink="https://editor.soumya-jit.tech/"              
+              description="My Pizza, a cutting-edge web app, now features two sides: Admin and Client. On the Admin side, restaurant managers take control with CRUD operations and reservation management. Meanwhile, the Client side offers users a seamless experience, with easy menu navigation and a user-friendly reservation system. Feel free to connect with me and share your thoughts! 🍕💻"
+              ghLink="https://github.com/MeriemMerbouhi/restaurant.git            
             />
           </Col>
-
-          {/* <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col> */}
         </Row>
       </Container>
     </Container>
